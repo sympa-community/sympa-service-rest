8983f73850a71ece82388f5ed8e955e7c3ca20df
Stefan Hornburg (Racke) <racke@linuxia.de>
2017-04-01 17:16:29 +0200

    Add abstract and authors to main POD.

--- lib/Sympa/API/REST.pm
+++ lib/Sympa/API/REST.pm
@@ -1,10 +1,26 @@
 package Sympa::API::REST;
 use Dancer2;
 
+=encoding utf8
+
+=head1 NAME
+
+Sympa::API::REST - REST API for Sympa Mailinglist Manager
+
+=cut
+
 our $VERSION = '0.1';
 
 get '/' => sub {
     template 'index' => { 'title' => 'Sympa::API::REST' };
 };
+
+=head1 AUTHORS
+
+Sympa Community C<sympa-developpers@listes.renater.fr>
+
+Stefan Hornburg (Racke), C<racke@linuxia.de>
+
+=cut
 
 true;
